e75315e29eabc871f85c10c67734cfb875cd4a6c
qa-artem <artem1305@yandex.ru>
2023-03-20 17:16:18 +0300

    done 2 tasks

---
+++ src/main/python/task1/Soda.py
@@ -0,0 +1,17 @@
+class Soda:
+
+    def __init__(self, ingredient):
+        if isinstance(ingredient, str):
+            self.ingredient = ingredient
+        else:
+            self.ingredient = None
+
+    def show_my_drink(self):
+        if self.ingredient:
+            print(f'Газировка и {self.ingredient}')
+        else:
+            print('Обычная газировка')
+
+
+drink = Soda()
+drink.show_my_drink()
